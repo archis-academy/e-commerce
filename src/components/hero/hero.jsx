12df8d5f18dbc899a6d34481f892b2b662825c8c
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-11-16 20:19:33 +0300

    html + css finished

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,4 +1,22 @@
+import { faArrowLeftLong, faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react"
 import "./hero.scss"
 export default function Hero(){
-    return ( <div className="caurosel"> Hero component works </div> )
+
+    const [backgroundImg , setbackgroundImg]= useState("/assets/CauroselSlider1.jpg");
+
+    return ( 
+    <div className="caurosel" style={{backgroundImage : `url(${backgroundImg})`}}>
+        <div className="bottomBar">
+            <div className="scroll">
+                <div className="scrollText"> SCROLL</div>
+                <span className="scrollLine"></span>
+            </div>
+            <div className="navigation">
+                <button><img src="/assets/CauroselNavigationButtonLeft.png" alt="" /></button>
+                <button><img src="/assets/CauroselNavigationButtonRight.png" alt=""/></button>
+            </div>
+        </div>
+    </div> )
 }
