9fa8b459e481cdb1874f5b92e29857b0bf70e968
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-11-16 16:09:18 +0300

    top bar developed

--- src/components/top-bar/top-bar.jsx
+++ src/components/top-bar/top-bar.jsx
@@ -1,3 +1,15 @@
-export default function TopBar(){
-    return ( <div> Topbar component works </div> )
+import "./top-bar.scss";
+export default function TopBar() {
+  
+  return (
+    <div className="topBar">
+      <div className="text">TÜM SİPARİŞLERDE ÜCRETSİZ KARGO</div>
+      <div className="language">
+        TR
+        <button>
+          <img src="/assets/DownIcon.png" />
+        </button>
+      </div>
+    </div>
+  );
 }
